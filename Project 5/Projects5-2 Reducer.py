ceab3d2189a870b9658eb5b31cc6701b3cc5f293
upjohnc <upjohnc@gmail.colm>
2014-03-12 15:37:45 -0500

    Finished Exercise 2 of Proj 5

--- Project 5/Projects5-2 Reducer.py
+++ Project 5/Projects5-2 Reducer.py
@@ -15,16 +15,23 @@ def reducer():
     riders = 0
     old_key = None
 
-    sumValue = 0.0
-    daysInMonth = 31.0
-
     for line in sys.stdin:
 
         data = line.strip().split('\t')
 
-        sumValue += float(data[1])
+        if len(data) != 2:
+            continue
 
-    print '0\t{0}'.format(sumValue / daysInMonth)
+        this_key, count = data
+        if old_key and old_key != this_key:
+            print "{0}\t{1}".format(old_key, riders)
+            riders = 0
+
+        old_key = this_key
+        riders += float(count)
+
+    if old_key != None:
+        print "{0}\t{1}".format(old_key, riders)
 
 
 reducer()
